29a4fd23d436fe3579cbdf4649f25c1bf24b6fea
Хуршед <relix2294@gmail.com>
2015-09-29 16:16:55 +0600

    d

---
+++ src/com/javarush/test/level04/lesson16/home02/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level04.lesson16.home02;
+
+import java.io.*;
+
+/* Среднее такое среднее
+Ввести с клавиатуры три числа, вывести на экран среднее из них. Т.е. не самое большое и не самое маленькое.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)   throws Exception
+    {
+        //напишите тут ваш код
+    }
+}
