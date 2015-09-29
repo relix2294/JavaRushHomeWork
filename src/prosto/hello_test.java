e792a5506ead496f99c7d035e3314c9deb02fd6d
Хуршед <relix2294@gmail.com>
2015-09-29 16:20:26 +0600

    12

--- src/prosto/hello_test.java
+++ src/prosto/hello_test.java
@@ -7,6 +7,6 @@ public class hello_test
 {
     public static void main(String []args)
     {
-        System.out.println("‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡");
+        System.out.println("‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‚‚‚‡‡‡‡‡‡‡");
     }
 }
